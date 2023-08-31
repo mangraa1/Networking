61d58de169e17d7a7a9cf92d3dfac3c1630114c0
mangraa1 <georgiu.spare@gmail.com>
2023-08-31 11:18:33 +0300

    GET Request

--- Networking/Controllers/ViewController.swift
+++ Networking/Controllers/ViewController.swift
@@ -19,6 +19,28 @@ class ViewController: UIViewController {
     //MARK: - @IBActions
 
     @IBAction func getRequest(_ sender: Any) {
+
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+
+        let session = URLSession.shared
+
+        session.dataTask(with: url) { data, response, error in
+            guard let response = response,
+                  let data = data
+                  else { return }
+
+            print(response)
+            print(data)
+
+            // Converting data from one format to another to get data
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+
+        }.resume()
     }
 
     @IBAction func postRequest(_ sender: Any) {
