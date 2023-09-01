f32b90df4c92d2c3ad82f7a02a586688819529bd
mangraa1 <georgiu.spare@gmail.com>
2023-09-01 18:55:23 +0300

    POST request

--- Networking/Controllers/ViewController.swift
+++ Networking/Controllers/ViewController.swift
@@ -44,6 +44,31 @@ class ViewController: UIViewController {
     }
 
     @IBAction func postRequest(_ sender: Any) {
+
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+
+        let userData = ["Project": "Networking", "Commit": "Sixth"]
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData) else { return }
+        request.httpBody = httpBody
+
+        let session = URLSession.shared
+        session.dataTask(with: request) { data, response, error in
+            guard let response = response, let data = data else { return }
+
+            print(response)
+
+            do {
+                let json = try JSONSerialization.jsonObject(with: data)
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
 }
 
